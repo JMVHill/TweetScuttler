c23398216a398b7207ac8243381fb32c560c3d02
Steven Job <steven_job@live.com>
2016-03-27 20:19:14 +0100

    Setup netty socket io on the backend in java

--- src/main/java/tweetscuttler/App.java
+++ src/main/java/tweetscuttler/App.java
@@ -1,10 +1,34 @@
 package tweetscuttler;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
+
+    @Value("${wss.server.host}")
+    private String host;
+
+    @Value("${wss.server.port}")
+    private Integer port;
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        Configuration config = new Configuration();
+        config.setHostname(host);
+        config.setPort(port);
+        return new SocketIOServer(config);
+    }
+
+    @Bean
+    public SpringAnnotationScanner springAnnotationScanner(SocketIOServer ssrv) {
+        return new SpringAnnotationScanner(ssrv);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
--- src/main/java/tweetscuttler/Test.java
+++ src/main/java/tweetscuttler/Test.java
@@ -1,15 +1,10 @@
 package tweetscuttler;
 
-import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Test {
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public String greeting() {
